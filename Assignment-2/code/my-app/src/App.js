373059826109b8186bc8f5720bad960b70f3eec3
Nagu@126 <nagubabu126@gmail.com>
2022-02-09 00:12:28 -0800

    first commit on assignment2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -9,10 +9,49 @@ function App(props) {
 return(
   <div className="container">
   {/* Start your React code here */}
+  
+    <div className="profile-img-container">
+    <img className="profile" src={employee.profileImg} width="100" height="100" alt="profile image"/>
+    </div>
+    <div className='title'>
+      <h1>{employee.name}</h1>
+    </div>
+    <div className="profile-info">
+    <label>Location</label>
+    <strong>{employee.location}</strong>
+    </div>
+    <div className="profile-info">
+      <label>Blood Group</label>
+      <strong>{employee.bloodGroup}</strong>
+    </div>
+    <div className="profile-info">
+      <label>Age</label>
+      <strong>{employee.age}</strong>
+
+    </div>
+
+
 </div>
-)
+
+);
+
 }
-
-
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://youtu.be/iZhV0bILFb0
+// https://youtu.be/iZhV0bILFb0
+// https://youtu.be/ly3m6mv5qvg
+// https://youtu.be/ly3m6mv5qvg
