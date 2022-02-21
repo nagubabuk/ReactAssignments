199459f10c7345f841306bf4f73149aeeaaa6404
Duleep <duleep.daniel@gmail.com>
2022-02-21 17:31:16 +0530

    Revert "First commit on Assignement4"
    This reverts commit b1d6aef3d9d7b7afe2c7e8185a25f77e03bb081e.

--- Assignment-4/code/src/index.js
+++ Assignment-4/code/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Postview from './Postview';
-import './Postview.css'
 import reportWebVitals from './reportWebVitals';
 
 
